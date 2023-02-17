41272e70537398478d9936a8ad360c3b9ce4a493
meslall <meslalla90@gmail.com>
2023-02-17 20:48:08 +0100

    ADD_ parse_config_file <SERVER>

--- lexer.hpp
+++ lexer.hpp
@@ -8,7 +8,7 @@
 #include<fstream>
 #include<list>
 #include<stack>
-
+#include<map>
 
 class Lexer {
 public:
@@ -39,11 +39,20 @@ typedef struct s_config {
     std::list<std::string> port;
 	size_t body_size_limit;
 	std::list<std::string> index;
-	std::string default_error_page;
+	std::map<std::string,std::string> default_error_page;
+    bool autoindex;
 	std::string root;
 	// std::list<t_location> locations;
 } t_config ;
 
 bool is_number(const std::string& s);
+void parse_host(Lexer &lexer,t_config &config);
+void parse_port(Lexer &lexer,t_config &config);
+void parse_client_max_body_size(Lexer &lexer,t_config &config);
+void parse_index(Lexer &lexer,t_config &config);
+void parse_error_page(Lexer &lexer,t_config &config);
+void parse_root(Lexer &lexer,t_config &config);
+void parse_auto_index(Lexer &lexer,t_config &config);
+void parse_location(Lexer &lexer,t_config &config);
 
 #endif
--- parse.cpp
+++ parse.cpp
@@ -12,68 +12,15 @@ bool    is_type(std::string type) {
     return false;
 }
 
-void parse_host(Lexer &lexer,t_config &config) {
 
-    if(lexer.get_size_value() > 1)
-        throw std::runtime_error("Error: host can only have one value");
-    config.host = lexer.getvalue(0);
-}
-
-void parse_port(Lexer &lexer,t_config &config) {
-
-    if(lexer.get_size_value() > 3)
-        throw std::runtime_error("Error: port can only have three values");
-    
-    for(int i = 0; i < lexer.get_size_value(); i++)
-    {
-        if(!is_number(lexer.getvalue(i)) && lexer.getvalue(i) != "")
-        {
-            if(lexer.getvalue(i) == "")
-            throw std::runtime_error("Error: port can only have numbers");
-        }
-        config.port.push_back(lexer.getvalue(i));
-    }
-}
-
-void parse_client_max_body_size(Lexer &lexer,t_config &config) {
-
-    if(lexer.get_size_value() > 1)
-        throw std::runtime_error("Error: client_max_body_size can only have one value");
-    if(!is_number(lexer.getvalue(0)))
-        throw std::runtime_error("Error: client_max_body_size can only have numbers");
-    config.body_size_limit = std::stoi(lexer.getvalue(0));
-}
-
-void parse_index(Lexer &lexer,t_config &config) {
-
-    if(lexer.get_size_value() > 3)
-        throw std::runtime_error("Error: index can only have three values");
-    
-    for(int i = 0; i < lexer.get_size_value(); i++)
-        config.index.push_back(lexer.getvalue(i));
-}
-
-void parse_error_page(Lexer &lexer,t_config &config) {
-
-    if(lexer.get_size_value() > 1)
-        throw std::runtime_error("Error: error_page can only have one value");
-    config.default_error_page = lexer.getvalue(0);
-}
-
-void parse_root(Lexer &lexer,t_config &config) {
-
-    if(lexer.get_size_value() > 1)
-        throw std::runtime_error("Error: root can only have one value");
-    config.root = lexer.getvalue(0);
-}
 
 void init_config(std::list<t_config> &cfg) {
     t_config config;
 
     config.host = "";
     config.body_size_limit = 0;
-    config.default_error_page = "";
     config.root = "";
+    config.autoindex = false;
     cfg.push_back(config);
 }
 
@@ -82,8 +29,10 @@ std::list<t_config>    parse_config(std::string filename) {
     Lexer lexer(filename);
     std::list<t_config> config;
 
-    std::string types[] = {"host", "port", "client_max_body_size", "index", "error_page", "root", "location"};
-    void    (*foo[]) (Lexer &,t_config &) = {parse_host , parse_port, parse_client_max_body_size, parse_index, parse_error_page, parse_root};
+    std::string types[] = {"host", "port", "client_max_body_size", "index", \
+    "error_page", "root", "location", "autoindex"};
+    void    (*foo[]) (Lexer &,t_config &) = {parse_host , parse_port, parse_client_max_body_size, parse_index, \
+        parse_error_page, parse_root, parse_location, parse_auto_index};
     
     while (lexer.gettype() != "EOF"){
         lexer.get_token();
@@ -93,7 +42,7 @@ std::list<t_config>    parse_config(std::string filename) {
                 init_config(config);
             continue;
         }
-        for(int i = 0 ; i < 6; i++)
+        for(int i = 0 ; i < 8; i++)
         {
             if(lexer.gettype() == types[i]){
                 foo[i](lexer, config.back());
@@ -116,8 +65,9 @@ int main( ){
            std::cout << "port: " << config.front().port.front() << std::endl;
            std::cout << "client_max_body_size: " << config.front().body_size_limit << std::endl;
            std::cout << "index: " << config.front().index.front() << std::endl;
-           std::cout << "error_page: " << config.front().default_error_page << std::endl;
+           std::cout << "error_page: " << config.front().default_error_page["404"] << std::endl;
            std::cout << "root: " << config.front().root << std::endl;
+           std::cout << "autoindex: " << config.front().autoindex << std::endl;
            config.pop_front();
        }
     }
